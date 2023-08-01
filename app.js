eef24b71f6036c4cce4f196a80793f136bb372f5
Miles-Collins <miles.collins@ymail.com>
2023-08-01 11:21:23 -0600

    added animals array, and draw animals

---
+++ app.js
@@ -0,0 +1,80 @@
+
+// SECTION ANIMALS ARRAY
+let animals = [
+  {
+    mammal: true,
+    canFly: false,
+    emoji: "🐖",
+  },
+  {
+    mammal: true,
+    canFly: false,
+    emoji: "🐒",
+  },
+  {
+    mammal: true,
+    canFly: false,
+    emoji: "🦍",
+  },
+  {
+    mammal: true,
+    canFly: false,
+    emoji: "🦏",
+  },
+  {
+    mammal: true,
+    canFly: true,
+    emoji: "🦇",
+  },
+  {
+    mammal: false,
+    canFly: true,
+    emoji: "🦚",
+  },
+  {
+    mammal: false,
+    canFly: true,
+    emoji: "🦩",
+  },
+  {
+    mammal: false,
+    canFly: false,
+    emoji: "🐟",
+  },
+  {
+    mammal: false,
+    canFly: false,
+    emoji: "🐳",
+  },
+  {
+    mammal: false,
+    canFly: false,
+    emoji: "🐢",
+  },
+  {
+    mammal: false,
+    canFly: false,
+    emoji: "🐍",
+  },
+  {
+    mammal: false,
+    canFly: true,
+    emoji: "🐝",
+  },
+  {
+    mammal: false,
+    canFly: false,
+    emoji: "🦂",
+  },
+  {
+    mammal: false,
+    canFly: false,
+    emoji: "🌵",
+  },
+];
+
+function drawAnimals(arrayOfAnimals) {
+  let animalString = ""
+  
+}
+
